fd58984714134819e86533a5129926f8d5c936cd
Zsuró Tibor <zsurotibor@gmail.com>
2020-12-29 18:25:59 +0100

    Add alert and prompt function to tiled module

--- src/types/tiled.d.ts
+++ src/types/tiled.d.ts
@@ -2,9 +2,13 @@ interface Tiled {
 
     activeAsset : Asset;
 
+    alert(text: string, title?: string): void;
+
     extendMenu(id: string, items: Array<any> | any) : void;
 
     log(text: string);
+
+    prompt(label: string, text?: string, title?: string): string;
 
     registerAction(id: string, callback: () => void): Action;
 }
